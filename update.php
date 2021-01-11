b447765004f213bdd276b163fdc87b08f9cf8975
Saul <saul.nunez18@tectijuana.edu.mx>
2021-01-11 10:09:01 -0800

    Avance
    Corregi el fallo de modificaciones del archivo update.php e inserte un nuevo archivo que es admin.php

--- update.php
+++ update.php
@@ -5,18 +5,26 @@ if(isset($_SESSION['logged_state']) && isset($_SESSION['permiso']))
 {
     if($_SESSION['permiso'] == 1)
     {
+
         $link = new mysqli(HOST, USER, PASS, DATA);
         $valor = 0;
         if(isset($_POST['btnAceptar']))
+
         {
+
             $valor = $_POST['btnAceptar'];
             $link->query("UPDATE tb_pcurriculares SET Estado = 1 WHERE NoUsuario = $valor");
+
         }
+
         else if(isset($_POST['btnDenegar']))
+
         {
+
             $valor = $_POST['btnDenegar'];
             $link->query("UPDATE tb_pcurriculares SET Estado = 2 WHERE NoUsuario = $valor");
         }
+
         header('location: admin.php');
     }
 }
