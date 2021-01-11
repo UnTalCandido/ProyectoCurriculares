344d6124f5d7bdec1b35a2de084b20736797e5c8
Carolina <carolina.dominguez18@tectijuana.edu.mx>
2021-01-11 01:06:25 -0800

    Avance
    Realize cambios los archivos consultas.php y curriculares.php

--- consultas.php
+++ consultas.php
@@ -3,8 +3,12 @@ session_start();
 require('./config.php');
 if(isset($_SESSION['logged_state']) && isset($_SESSION['permiso']))
 {
+
+
     if($_SESSION['permiso'] == 1)
     {
+
+
         $datos = "";
         $link = new mysqli(HOST, USER, PASS, DATA);
         $r = $link->query("SELECT tb_curriculares.Nombre AS Curricular, tb_grupos.Cupo - (SELECT COUNT(*) FROM tb_alumnosc WHERE tb_alumnosc.NoGrupo = tb_grupos.NoCurricular) AS Cupo, (SELECT COUNT(*) FROM tb_alumnosc WHERE tb_alumnosc.NoGrupo = tb_grupos.NoCurricular) AS Inscritos FROM tb_grupos INNER JOIN tb_curriculares ON tb_grupos.NoCurricular = tb_curriculares.ID");
@@ -14,6 +18,9 @@ if(isset($_SESSION['logged_state']) && isset($_SESSION['permiso']))
             "<td align=\"center\">".$result['Cupo']."</td>".
             "<td align=\"center\">".$result['Inscritos']."</td>".
             "</tr>";
+
+
+
         }
         $html = file_get_contents('./views/consultas.html');
         $html = str_replace('{{ datos }}', $datos, $html);
@@ -22,6 +29,7 @@ if(isset($_SESSION['logged_state']) && isset($_SESSION['permiso']))
 }
 else
 {
+
     echo "FORBIDDEN";
 }
 ?>
