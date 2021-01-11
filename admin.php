6c8d7ed75224c9ef0e8eff29d3e7935f779b3b91
Carolina <carolina.dominguez18@tectijuana.edu.mx>
2021-01-11 01:08:23 -0800

    Avance

--- admin.php
+++ admin.php
@@ -5,11 +5,13 @@ if(isset($_SESSION['logged_state']) && isset($_SESSION['permiso']))
 {
     if($_SESSION['permiso'] == 1)
     {
+
         $datos = "";
         $link = new mysqli(HOST, USER, PASS, DATA);
         $r = $link->query("SELECT tb_pcurriculares.NoUsuario AS Usuario, tb_usuarios.User as Matricula, tb_usuarios.nombre AS Nombre, tb_tipoespecial.Nombre AS Tipo, tb_pcurriculares.especificacion as Descripcion FROM `tb_pcurriculares` INNER JOIN tb_usuarios ON tb_pcurriculares.NoUsuario = tb_usuarios.ID INNER JOIN tb_tipoespecial ON tb_pcurriculares.id = tb_tipoespecial.ID WHERE tb_pcurriculares.especial > 0 AND tb_pcurriculares.Estado = 0");
         while($result = $r->fetch_array()){
-            $datos .= "<tr>".
+            
+	    $datos .= "<tr>".
             "<form method='POST' action='update.php'>".
             "<td>".$result['Matricula']."</td>".
             "<td>".$result['Nombre']."</td>".
@@ -19,10 +21,13 @@ if(isset($_SESSION['logged_state']) && isset($_SESSION['permiso']))
             "<td><button name='btnDenegar' value='".$result['Usuario']."'>Denegar</button></td>".
             "</form>".
             "</tr>";
+
         }
+
         $html = file_get_contents('./views/admin.html');
         $html = str_replace('{{ datos }}', $datos, $html);
         echo $html;
+
     }
 }
 else
